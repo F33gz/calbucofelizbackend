383fc857bd97e71a19086a28202943b7e7b03601
Alvaro Parra <paparrasilva2@gmail.com>
2025-05-19 02:10:19 -0400

    feat: init project

---
+++ src/main/java/cl/metspherical/calbucofelizbackend/CalbucoFelizBackendApplication.java
@@ -0,0 +1,13 @@
+package cl.metspherical.calbucofelizbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalbucoFelizBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalbucoFelizBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/metspherical/calbucofelizbackend/CalbucoFelizBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.metspherical.calbucofelizbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalbucoFelizBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
