6d06c8a113c2f8bbda9786625e70ebcc022a0f3d
bfl-dev <beyonfl@gmail.com>
2025-06-05 23:05:57 -0400

    feat: expand PostDetailDTO to include id, likes, and comments

--- src/main/java/cl/metspherical/calbucofelizbackend/dto/PostDetailDTO.java
+++ src/main/java/cl/metspherical/calbucofelizbackend/dto/PostDetailDTO.java
@@ -2,13 +2,16 @@ package cl.metspherical.calbucofelizbackend.dto;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
 public record PostDetailDTO(
-    String content,
-    LocalDateTime createdAt,
-    AuthorDTO author,
-    List<PostImageDTO> images,
-    List<CategoryDTO> categories
-) {
-}
+        UUID id,
+        String content,
+        LocalDateTime createdAt,
+        AuthorDTO author,
+        List<String> images,
+        List<CategoryDTO> categories,
+        Integer likes,
+        Integer comments
+) { }
 
