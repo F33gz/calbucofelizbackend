7e9581350f5cd293f93b4f70959cf85ea267dfda
F33gz <127645044+F33gz@users.noreply.github.com>
2025-06-18 16:15:07 -0400

    feat: add JwtAuthenticationToken class to implement Authentication interface

---
+++ src/main/java/cl/metspherical/calbucofelizbackend/security/JwtAuthenticationToken.java
@@ -0,0 +1,50 @@
+package cl.metspherical.calbucofelizbackend.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+public record JwtAuthenticationToken(UUID userId,String username, List<String> roles) implements Authentication {
+
+    @Override
+    public String getName() {
+        return username;
+    }
+
+    @Override
+    public UUID getPrincipal() {
+        return userId;
+    }
+
+    @Override
+    public boolean isAuthenticated() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getDetails() {
+        return null;
+    }
+
+    @Override
+    public void setAuthenticated(boolean isAuthenticated) {
+        throw new UnsupportedOperationException();
+    }
+}
